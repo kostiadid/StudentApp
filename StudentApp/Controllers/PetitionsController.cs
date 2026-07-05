75e7aa1bc55b3457eef1b0af96046fda71a2e6f9
Kostia Borysenko <Kostia.Borysenko@Lifemark.ca>
2026-07-05 18:03:17 -0400

    API   /  now will work on services  /   I know we need to split  interface  and  service in  to a different files  .. but for this size of project  I will  not do it .

---
+++ StudentApp/Controllers/PetitionsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentApp.Database;
+using StudentApp.Entities;
+
+namespace StudentApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PetitionsController : ControllerBase
+    {
+        private readonly StudentDbContext _context;
+
+        public PetitionsController(StudentDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreatePetition(Petition petition)
+        {
+            petition.Status = PetitionStatus.Draft;
+            petition.CreatedAt = DateTime.UtcNow;
+            _context.Petitions.Add(petition);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetPetitionById), new { id = petition.Id }, petition);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPetitionById(int id)
+        {
+            var petition = _context.Petitions.Find(id);
+            if (petition == null) return NotFound();
+            return Ok(petition);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPetitions()
+        {
+            var petitions = _context.Petitions.ToList();
+            return Ok(petitions);
+        }
+    }
+
+}
---
+++ StudentApp/Controllers/StudentsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentApp.Database;
+using StudentApp.Entities;
+
+namespace StudentApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly StudentDbContext _context;
+
+        public StudentsController(StudentDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetStudentById), new { id = student.Id }, student);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudentById(int id)
+        {
+            var student = _context.Students.Find(id);
+            if (student == null) return NotFound();
+            return Ok(student);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            var students = _context.Students.ToList();
+            return Ok(students);
+        }
+
+    }
+}
+
